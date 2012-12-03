d50930f266ad8e57c511ee851b48567a16f3ab30
Jiaqing Sun <kobeqing@gmail.com>
2012-12-03 18:48:09 +0800

    modify

--- ReportManager/src/com/ericsson/rm/service/ReportService.java
+++ ReportManager/src/com/ericsson/rm/service/ReportService.java
@@ -3,11 +3,11 @@ package com.ericsson.rm.service;
 import com.ericsson.rm.bean.TestReport;
 
 public class ReportService implements IReportService {
-
+	
 	public void addReport(TestReport testReport) {
 		String caseName = testReport.getCaseName();
 		String casePath = testReport.getCathPath();
 		String caseResult = testReport.getCaseResult();
 	}
-
+	
 }
