213135451f7a8d580dafdbb28efff955964fea9f
Jiaqing Sun <kobeqing@gmail.com>
2012-12-03 23:15:47 +0800

    modify code home

--- ReportManager/src/com/ericsson/rm/service/ReportService.java
+++ ReportManager/src/com/ericsson/rm/service/ReportService.java
@@ -1,13 +1,24 @@
 package com.ericsson.rm.service;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+
 import com.ericsson.rm.bean.TestReport;
 
 public class ReportService implements IReportService {
+	
+	private static Logger logger = Logger.getLogger(ReportService.class);
 	
 	public void addReport(TestReport testReport) {
 		String caseName = testReport.getCaseName();
 		String casePath = testReport.getCathPath();
 		String caseResult = testReport.getCaseResult();
+		String logPath = testReport.getLogPath();
+		
+		if (caseName == null || caseName.isEmpty()) {
+			
+		}
+		
 	}
 	
 }
