6ae61ef6481a8459e52780b8c6499baeccae024a
ejiaqsu <jiaqing.sun@ericsson.com>
2013-07-01 08:26:21 +0800

    mmm

--- android/Chapter7/gen/com/example/chapter7/R.java
+++ android/Chapter7/gen/com/example/chapter7/R.java
@@ -10,6 +10,10 @@ package com.example.chapter7;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f060000;
+        public static final int background2=0x7f060001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -36,19 +40,31 @@ public final class R {
         public static final int menu5icon=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int button_one=0x7f080000;
+        public static final int button_one=0x7f090001;
+        public static final int buttonfive=0x7f090008;
+        public static final int buttonfour=0x7f090007;
+        public static final int buttonone=0x7f090004;
+        public static final int buttonthree=0x7f090006;
+        public static final int buttontwo=0x7f090005;
+        public static final int imageView1=0x7f090003;
+        public static final int textView1=0x7f090002;
+        public static final int uilayout=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int showalert=0x7f050007;
+        public static final int showblack=0x7f050006;
+        public static final int showimage1=0x7f050003;
+        public static final int showimage2=0x7f050004;
+        public static final int showwhite=0x7f050005;
     }
     public static final class style {
         /** 
@@ -72,10 +88,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
