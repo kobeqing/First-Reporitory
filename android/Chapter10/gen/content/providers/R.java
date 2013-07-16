3bf8ee468bad79b961f20d3d956f5a2dcd364f3b
ejiaqsu <jiaqing.sun@ericsson.com>
2013-07-17 07:59:30 +0800

    chapter10

--- android/Chapter10/gen/content/providers/R.java
+++ android/Chapter10/gen/content/providers/R.java
@@ -24,7 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int addContactButton=0x7f080001;
+        public static final int deleteContactButton=0x7f080003;
+        public static final int modifyPhoneButton=0x7f080002;
+        public static final int queryButton=0x7f080000;
     }
     public static final class layout {
         public static final int activity_data_examples=0x7f030000;
